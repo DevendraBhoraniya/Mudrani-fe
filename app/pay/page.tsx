9f29f9d7919c4431492d4cd2557de1eb664e9eb0
DevendraBhoraniya <bhoraniyadevendra@gmail.com>
2023-08-27 14:43:45 +0530

    feat: add pay page for payment

---
+++ app/pay/page.tsx
@@ -0,0 +1,12 @@
+import Tabs from '@/components/Tab'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mx-[10%]'>
+      <Tabs />
+    </div>
+  )
+}
+
+export default page
