8302c6979451ec6b96fa1fce7496574b1512a341
DevendraBhoraniya <bhoraniyadevendra@gmail.com>
2023-08-27 14:47:01 +0530

    Feat: added navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import '@rainbow-me/rainbowkit/styles.css';
 
 import { Providers } from './provider';
+import Navbar from '@/components/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -20,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          {/* <Navbar /> */}
+          <Navbar />
           <main>{children}</main>
           {/* <Footer /> */}
         </Providers>
