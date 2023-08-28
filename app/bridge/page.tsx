b5b52b647bccf66119ac0114d8e29da7eebd681d
Goraniya Karan <karangoraniya@gmail.com>
2023-08-28 23:55:39 +0530

    add: add bridge integration added 🚀

---
+++ app/bridge/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+import Tabs from '@/components/Tab';
+import React from 'react';
+
+const Bridge = () => {
+  //   const { config } = usePrepareContractWrite({
+  //     address: contractAddr,
+  //     abi: mudraniABI,
+  //     functionName: 'xTransfer',
+
+  //     args: [
+  //       //   0.01,
+  //     ],
+  //   });
+
+  return (
+    <div>
+      <Tabs />
+    </div>
+  );
+};
+
+export default Bridge;
